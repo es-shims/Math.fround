es-shims/Math.cosh

Implement Math.cosh polyfill

test/implementation.js
@@ -1,0 +1,20 @@
+'use strict';
+
+var test = require('tape');
+var functionsHaveNames = require('functions-have-names')();
+
+var implementation = require('../implementation');
+var runTests = require('./tests');
+
+test('implementation', function (t) {
+	t.equal(implementation.length, 1, 'implementation has a length of 1');
+
+	t.test('Function name', { skip: !functionsHaveNames }, function (st) {
+		st.equal(implementation.name, 'cosh', 'implementation has name "cosh"');
+		st.end();
+	});
+
+	runTests(implementation, t);
+
+	t.end();
+});

test/index.js
@@ -1,0 +1,11 @@
+'use strict';
+
+var cosh = require('../');
+var test = require('tape');
+var runTests = require('./tests');
+
+test('as a function', function (t) {
+	runTests(cosh, t);
+
+	t.end();
+});

test/index.mjs
@@ -1,0 +1,24 @@
+import cosh, * as coshModule from 'math.cosh';
+import test from 'tape';
+import runTests from './tests.js';
+
+test('as a function', (t) => {
+	runTests(cosh, t);
+
+	t.end();
+});
+
+test('named exports', async (t) => {
+	t.deepEqual(
+		Object.keys(coshModule).sort(),
+		['default', 'shim', 'getPolyfill', 'implementation'].sort(),
+		'has expected named exports',
+	);
+
+	const { shim, getPolyfill, implementation } = coshModule;
+	t.equal(await import('math.cosh/shim'), shim, 'shim named export matches deep export');
+	t.equal(await import('math.cosh/implementation'), implementation, 'implementation named export matches deep export');
+	t.equal(await import('math.cosh/polyfill'), getPolyfill, 'getPolyfill named export matches deep export');
+
+	t.end();
+});

test/shimmed.js
@@ -1,0 +1,28 @@
+'use strict';
+
+require('../shim')();
+
+var test = require('tape');
+var defineProperties = require('define-properties');
+var isEnumerable = Object.prototype.propertyIsEnumerable;
+var functionsHaveNames = require('functions-have-names')();
+
+var runTests = require('./tests');
+
+test('shimmed', function (t) {
+	t.equal(Math.cosh.length, 1, 'Math.cosh has a length of 1');
+
+	t.test('Function name', { skip: !functionsHaveNames }, function (st) {
+		st.equal(Math.cosh.name, 'cosh', 'Math.cosh has name "cosh"');
+		st.end();
+	});
+
+	t.test('enumerability', { skip: !defineProperties.supportsDescriptors }, function (et) {
+		et.equal(false, isEnumerable.call(Math, 'cosh'), 'Math.cosh is not enumerable');
+		et.end();
+	});
+
+	runTests(Math.cosh, t);
+
+	t.end();
+});

es-shims/Math.fround

test/implementation.js
@@ -1,0 +1,20 @@
+'use strict';
+
+var test = require('tape');
+var functionsHaveNames = require('functions-have-names')();
+
+var implementation = require('../implementation');
+var runTests = require('./tests');
+
+test('implementation', function (t) {
+	t.equal(implementation.length, 1, 'implementation has a length of 1');
+
+	t.test('Function name', { skip: !functionsHaveNames }, function (st) {
+		st.equal(implementation.name, 'fround', 'implementation has name "fround"');
+		st.end();
+	});
+
+	runTests(implementation, t);
+
+	t.end();
+});

test/index.js
@@ -1,0 +1,11 @@
+'use strict';
+
+var fround = require('../');
+var test = require('tape');
+var runTests = require('./tests');
+
+test('as a function', function (t) {
+	runTests(fround, t);
+
+	t.end();
+});

test/index.mjs
@@ -1,0 +1,24 @@
+import fround, * as froundModule from 'math.fround';
+import test from 'tape';
+import runTests from './tests.js';
+
+test('as a function', (t) => {
+	runTests(fround, t);
+
+	t.end();
+});
+
+test('named exports', async (t) => {
+	t.deepEqual(
+		Object.keys(froundModule).sort(),
+		['default', 'shim', 'getPolyfill', 'implementation'].sort(),
+		'has expected named exports',
+	);
+
+	const { shim, getPolyfill, implementation } = froundModule;
+	t.equal(await import('math.fround/shim'), shim, 'shim named export matches deep export');
+	t.equal(await import('math.fround/implementation'), implementation, 'implementation named export matches deep export');
+	t.equal(await import('math.fround/polyfill'), getPolyfill, 'getPolyfill named export matches deep export');
+
+	t.end();
+});

test/shimmed.js
@@ -1,0 +1,28 @@
+'use strict';
+
+require('../shim')();
+
+var test = require('tape');
+var defineProperties = require('define-properties');
+var isEnumerable = Object.prototype.propertyIsEnumerable;
+var functionsHaveNames = require('functions-have-names')();
+
+var runTests = require('./tests');
+
+test('shimmed', function (t) {
+	t.equal(Math.fround.length, 1, 'Math.fround has a length of 1');
+
+	t.test('Function name', { skip: !functionsHaveNames }, function (st) {
+		st.equal(Math.fround.name, 'fround', 'Math.fround has name "fround"');
+		st.end();
+	});
+
+	t.test('enumerability', { skip: !defineProperties.supportsDescriptors }, function (et) {
+		et.equal(false, isEnumerable.call(Math, 'fround'), 'Math.fround is not enumerable');
+		et.end();
+	});
+
+	runTests(Math.fround, t);
+
+	t.end();
+});